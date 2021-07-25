a2b3398aad38a8cbba7d614f2802b7cfbdf10e94
Rutuja Chitra-Tarak <arutuj@gmail.com>
2021-07-24 18:57:18 -0600

    Turned off section to initialise conda at login

--- 1.FATES-MODEL/2.create.conda.env.sh
+++ 1.FATES-MODEL/2.create.conda.env.sh
@@ -4,26 +4,26 @@
 # Rutuja Chitra-Tarak
 # July 15, 2021
 #"""
-module avail python
-module load python/3.8-anaconda-2020.07
+##**************************************************************
+# Uncomment this during the first instance of running this script
+#module load python/3.8-anaconda-2020.07
 
-module avail python
+#module avail python
 
-conda info
+#conda info
 
-# To check installed conda environments
-conda env list
-# OR
-# conda info --envs
+## To check installed conda environments
+# conda env list
+## OR
+## conda info --envs
 
-# To initialise conda
-source "${LPYTHON_HOME}/etc/profile.d/conda.csh"
+## To initialise conda
+# source "${LPYTHON_HOME}/etc/profile.d/conda.csh"
 
-# To initialise conda at login
+## To initialise conda at login
 # module load python/3.8-anaconda-2020.07
-# conda init
-# move the contents of the .bashrc file to .tcshrc
-# This will also allow for conda activate to work
+# conda init tcsh
+##**************************************************************
 
 # add the curret directory to the conda env dir
 set currentdir=`pwd`
@@ -31,8 +31,11 @@ conda config --add envs_dirs $currentdir
 # To remove:
 # conda config --remove envs_dirs $currentdir 
 
+# To remove older conda env
+conda remove conda_env
+
 # Create a conda environment with a specified name, conda_env, and a desired list of modules and their versions
-conda create --name conda_env python=3.6 r-base=3.6 r-essentials=3.6 rpy2 pandas r-ncdf4
+conda create --name conda_env python=3.6 r-base=3.6 r-essentials=3.6 rpy2 pandas r-ncdf4 mpi4py
 
 # Activate the environment
 conda activate conda_env
@@ -50,6 +53,8 @@ conda install -c conda-forge tzlocal
 # Make sure the conda env is on the PATH
 echo $PATH
 set PATH=$currentdir'/conda_env/bin':$PATH
+echo $PATH
+
 # Activate the environment
 conda activate conda_env
 # -----End------
