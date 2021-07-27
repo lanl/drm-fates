5b5cac3864d98a4c376458c114df4acc43e8fbd1
Rutuja Chitra-Tarak <arutuj@gmail.com>
2021-07-26 22:22:46 -0600

    updated .sh filename

--- 1.FATES-MODEL/create.ELM.ensemble.py
+++ 1.FATES-MODEL/create.ELM.ensemble.py
@@ -38,5 +38,5 @@ BASE_CASE = "BCI.ICLM45ED.badger.intel.C700b46fec-F8c9cd1b0.met.v5.2016-2018"
 os.putenv('BASE_CASE', BASE_CASE)
 
 # This will clone a base case ending with elements of case_arr
-subprocess.call('./Generate_Sen_Cases.csh')
+subprocess.call('./create.ELM.ensemble.csh')
 
