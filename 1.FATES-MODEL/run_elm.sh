f85b65d8bdc7e561dc31ede5af535dda57f74d39
Rutuja Chitra-Tarak <arutuj@gmail.com>
2021-07-26 20:28:53 -0600

    added shebang and modules to run from backnode

--- 1.FATES-MODEL/run_elm.sh
+++ 1.FATES-MODEL/run_elm.sh
@@ -1,6 +1,31 @@
+#!/usr/bin/env bash
+#SBATCH -o slurm%j.out
+#SBATCH -e slurm%j.err
+#SBATCH -A w18_califorest
+#SBATCH -N 1 # number of nodes
+#SBATCH -t 1:00:00
 
 currentdir=`pwd`'/'
 echo 'Started on '`date`' for '$currentdir' starting in '$currentdir
+
+module purge
+module use /usr/projects/cesm/software/local/modulefiles/all
+module load  acmeenv/1.0.0
+#module load  clmedenv/1.0.0
+#module load  intel/15.0.5
+module load  intel/17.0.1
+#module load  openmpi/1.6.5
+#module load  openmpi
+#module load  mkl/10.3.11
+module load mkl
+#module load  cmake/3.5.2
+module load  ncview
+module load  nco/4.4.4
+module load  ncl
+#module load  python/2.7-anaconda-2.1.0
+module load python/anaconda-2.7-climate
+module load  mpi4py/3.0.0
+module load netcdf/4.4.1
 
 cd $currentdir
 
