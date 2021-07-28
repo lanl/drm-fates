fd719c0f2ce08cccd4bec5b0b2ef42f4fbfeae6c
Rutuja Chitra-Tarak <arutuj@gmail.com>
2021-07-28 08:44:50 -0600

    Scripts moved to src or tools subfolder

--- 1.FATES-MODEL/create.basecase.py
+++
@@ -1,9 +0,0 @@
-"""
-# Creates a base case using a shell script
-# Rutuja Chitra-Tarak
-# July 25, 2021
-"""
-
-import subprocess
-subprocess.call(['sh', './create.basecase.sh'])
-
--- 1.FATES-MODEL/run_elm.py
+++
@@ -1,8 +0,0 @@
-"""
-# Runs a shell script to run EM in parallel
-# Rutuja Chitra-Tarak
-# July 25, 2021
-"""
-
-import subprocess
-subprocess.call(['sh', './run_elm.sh'])
