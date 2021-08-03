d76aab19069c65f90378eee1ecb0a3a6fef624f6
Rutuja Chitra-Tarak <arutuj@gmail.com>
2021-08-02 22:54:19 -0600

    vars sourced from yaml

--- 1.FATES-MODEL/src/extract.output.py
+++ 1.FATES-MODEL/src/extract.output.py
@@ -24,25 +24,28 @@ with open(args.config_file, 'r') as in_file:
 dir_path = os.path.dirname(os.path.realpath(__file__))
 R_file = dir_path+'/extract.output.R'
 outdir = config_dict['PROJECT_ROOT']+'/'+config_dict['OUTPUT_DIR']
-cloneroot = "/lustre/scratch3/turquoise/rutuja/ACME/cases"
+runroot = config_dict['RUN_ROOT']
 
 # Defining the R script and loading the instance in Python
 r = robjects.r
 r['source'](R_file)
 
 # Loading the function we have defined in R.
-extractres_h0_r = robjects.globalenv['extractres_h0']
+#extractres_h0_r = robjects.globalenv['extractres_h0']
 extractres_h1_r = robjects.globalenv['extractres_h1']
-extractres_h2_r = robjects.globalenv['extractres_h2']
+#extractres_h2_r = robjects.globalenv['extractres_h2']
 
 sam_start = config_dict['HPU_ID_START']
 sam_end = config_dict['HPU_ID_END']
 
-#filebase = "BCI.ICLM45ED.badger.intel.C700b46fec-F8c9cd1b0.met.v5.2016-2018"
-filebase = "BCI.ICLM45ED.badger.intel.C700b46fec-F8c9cd1b0.full.met.v6."
+start_year = config_dict['DATM_CLMNCEP_YR_START']
+end_year = config_dict['DATM_CLMNCEP_YR_END']
 
-start_year = 2016
-end_year = 2018
+# Set the BASE CASE name. This is generated from yaml and src/create.basecase.sh
+ff=open(config_dict['PROJECT_ROOT']+"/BASE_CASE_NAME.txt", "r")
+BASE_CASE=ff.read()
+filebase = print(BASE_CASE.replace(str(start_year)+'-'+str(end_year),''))
+
 
 filterFile = "Filter.txt"
 var_vec_h1 = ["H2OSOI", "QRUNOFF"] 
@@ -64,7 +67,7 @@ with localconverter(robjects.default_converter + pandas2ri.converter):
 scale_vec_h1_r
 
 #Invoking the R function and getting the result. Note that the sequence of arguments is critical
-h1_result = extractres_h1_r(sam_start, sam_end, outdir, cloneroot, filebase, var_vec_h1_r, scale_vec_h1_r, filterFile, start_year, end_year)
+h1_result = extractres_h1_r(sam_start, sam_end, outdir, runroot, filebase, var_vec_h1_r, scale_vec_h1_r, filterFile, start_year, end_year)
 if (h1_result):
     print('Daily outputs extracted successfully for ', var_vec_h1)
     exit(0)
