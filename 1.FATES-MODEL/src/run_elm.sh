c569613ee604bdf833d55e39561e9ea02865973d
Rutuja Chitra-Tarak <arutuj@gmail.com>
2021-08-10 13:34:05 -0600

    modules uncommented for mpirun to work on back end nodes

--- 1.FATES-MODEL/src/run_elm.sh
+++ 1.FATES-MODEL/src/run_elm.sh
@@ -10,24 +10,24 @@ PY_SRC_PATH=`echo $2 | sed 's/^[ \t]*//;s/[ \t]*$//'`
 
 echo 'Started on '`date`' for '$casedir' starting in '$casedir
 
-#module purge
-#module use /usr/projects/cesm/software/local/modulefiles/all
-#module load  acmeenv/1.0.0
-##module load  clmedenv/1.0.0
-##module load  intel/15.0.5
-#module load  intel/17.0.1
-##module load  openmpi/1.6.5
-##module load  openmpi
-##module load  mkl/10.3.11
-#module load mkl
-##module load  cmake/3.5.2
-#module load  ncview
-#module load  nco/4.4.4
-#module load  ncl
-##module load  python/2.7-anaconda-2.1.0
-#module load python/anaconda-2.7-climate
-#module load  mpi4py/3.0.0
-#module load netcdf/4.4.1
+module purge
+module use /usr/projects/cesm/software/local/modulefiles/all
+module load  acmeenv/1.0.0
+#module load  clmedenv/1.0.0
+#module load  intel/15.0.5
+module load  intel/17.0.1
+#module load  openmpi/1.6.5
+#module load  openmpi
+#module load  mkl/10.3.11
+module load mkl
+#module load  cmake/3.5.2
+module load  ncview
+module load  nco/4.4.4
+module load  ncl
+#module load  python/2.7-anaconda-2.1.0
+module load python/anaconda-2.7-climate
+module load  mpi4py/3.0.0
+module load netcdf/4.4.1
 
 cd $casedir
 
