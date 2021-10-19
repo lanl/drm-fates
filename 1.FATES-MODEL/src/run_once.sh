56382aea62561f75f9d998f4451b9a62c967a03c
Rutuja Chitra-Tarak <arutuj@gmail.com>
2021-10-19 15:19:29 -0600

    corrected conda environment prefix and created conda env directory if not already present

--- 1.FATES-MODEL/src/run_once.sh
+++ 1.FATES-MODEL/src/run_once.sh
@@ -8,13 +8,15 @@ source "$SCRIPT_DIR/../tools/yaml.sh"
 create_variables "$SCRIPT_DIR/../config.yaml"
 
 PROJECT_ROOT=`realpath "$SCRIPT_DIR/.."`
+cd $PROJECT_ROOT
+mkdir -p elm_env # create if not already present
 
 # 2. To create the environment from a yml file, run this in a shell:
-conda config --add envs_dirs $PROJECT_ROOT
+conda config --add envs_dirs $PROJECT_ROOT/elm_env
 ## Name of the new conda env is conda_env in the environment.yml file. If such env elready xists, change the name in the file
 sed -i 's/conda_env/elm_env/g' environment.yml
 sed -i '/prefix/d' environment.yml
-sed -i -e '$a\' -e 'prefix: $PROJECT_ROOT/elm_env' environment.yml
+sed -i -e '$a\' -e "prefix: $PROJECT_ROOT/elm_env" environment.yml
 cd $PROJECT_ROOT
 conda env create -f environment.yml
 
