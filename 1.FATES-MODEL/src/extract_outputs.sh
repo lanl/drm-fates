da3444b736d343669d21f0e9078340236c0a84a1
Rutuja Chitra-Tarak <arutuj@gmail.com>
2021-12-03 15:31:48 -0700

    filter script moved to README

--- 1.FATES-MODEL/src/extract_outputs.sh
+++ 1.FATES-MODEL/src/extract_outputs.sh
@@ -6,12 +6,7 @@
 # Rutuja Chitra-Tarak (Tue Aug 3, 2021)
 # =======================================================================================
 
-# 6. To find which cases are complete (output/Filter.txt) and which are not (output/Missing.txt), run:
-
-python src/create.filter.py
-
- 
-# 7. To extract outputs from ELM ensembles (output/exrtact/elm_daily_outputs.txt), run:
+# To extract outputs from ELM ensembles (output/exrtact/elm_daily_outputs.txt), run:
 
 python src/extract.output.py
 
