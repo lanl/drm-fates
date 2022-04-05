a3b7ed7e4607b1b9eaf74ba35947fb44c4c833bf
Rutuja Chitra-Tarak <arutuj@gmail.com>
2022-04-05 05:01:09 -0600

    src/run_elm.sh for the current path

--- 1.FATES-MODEL/src/run_elm.sh
+++ 1.FATES-MODEL/src/run_elm.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 #SBATCH -o slurm%j.out
 #SBATCH -e slurm%j.err
-#SBATCH -A w18_califorest
+#SBATCH -A w22_fire
 #SBATCH -N 1 # number of nodes
 #SBATCH -t 1:00:00
 
 #casedir=`echo $1 | sed 's/^[ \t]*//;s/[ \t]*$//'`
 #PY_SRC_PATH=`echo $2 | sed 's/^[ \t]*//;s/[ \t]*$//'`
 
-casedir='/turquoise/usr/projects/veg/rutuja/ACME_cases/cimmid/CASE_DIR'
+casedir='/turquoise/usr/projects/higrad/rutuja/E3SM_cases/proj1/CASE_DIR'
 echo 'Started on '`date`' for '$casedir' starting in '$casedir
 
 module purge
@@ -28,7 +28,7 @@ cd $casedir
 date=`date "+%F-%T"`
 
 jobid=$SLURM_JOB_ID
-mpiexec -n 2 python /turquoise/usr/projects/veg/rutuja/ACME_cases/cimmid/src/parallel.run.py -c 'E3SMv2.IELM.badger.intel.Cead8f1ada2-F387082946.2010-2011.' -r /lustre/scratch4/turquoise/rutuja/E3SM/scratch -f clm2.h0.2010-12.nc -s 1 -t 2 -g /turquoise/usr/projects/veg/rutuja/ACME_cases/cimmid/log
+mpiexec -n 2 python /turquoise/usr/projects/higrad/rutuja/E3SM_cases/proj1/src/parallel.run.py -c 'E3SMv2.IELM.badger.intel.C60051649f4-F387082946.2010-2011.' -r /lustre/scratch4/turquoise/rutuja/E3SM/scratch -f clm2.h0.2010-12.nc -s 1 -t 2 -g /turquoise/usr/projects/higrad/rutuja/E3SM_cases/proj1/log
 
 
 #python $PY_SRC_PATH
