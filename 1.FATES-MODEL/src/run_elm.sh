6eff2d6f1648421862c3089207b76074bafd3175
Rutuja Chitra-Tarak <arutuj@gmail.com>
2022-04-20 19:56:56 -0600

    Removed module loading within run_elm.sh

--- 1.FATES-MODEL/src/run_elm.sh
+++ 1.FATES-MODEL/src/run_elm.sh
@@ -8,27 +8,27 @@
 #casedir=`echo $1 | sed 's/^[ \t]*//;s/[ \t]*$//'`
 #PY_SRC_PATH=`echo $2 | sed 's/^[ \t]*//;s/[ \t]*$//'`
 
-casedir='/turquoise/usr/projects/cimmid/users/rutuja/E3SM_cases/proj1/CASE_DIR'
+casedir='/turquoise/usr/projects/higrad/rutuja/DRM/1.FATES-MODEL/CASE_DIR'
 echo 'Started on '`date`' for '$casedir' starting in '$casedir
 
-module purge
-module use /usr/projects/cesm/software/local/modulefiles/all
-module load  acmeenv/1.0.0
-module load  intel/17.0.1
-module load mkl
-module load  ncview
-module load  nco/4.4.4
-module load  ncl
-module load python/anaconda-2.7-climate
-module load  mpi4py/3.0.0
-module load netcdf/4.4.1
+#module purge
+#module use /usr/projects/cesm/software/local/modulefiles/all
+#module load  acmeenv/1.0.0
+#module load  intel/17.0.1
+#module load mkl
+#module load  ncview
+#module load  nco/4.4.4
+#module load  ncl
+#module load python/anaconda-2.7-climate
+#module load  mpi4py/3.0.0
+#module load netcdf/4.4.1
 
 cd $casedir
 
 date=`date "+%F-%T"`
 
 jobid=$SLURM_JOB_ID
-mpiexec -n 2 python /turquoise/usr/projects/cimmid/users/rutuja/E3SM_cases/proj1/src/parallel.run.py -c 'E3SMv2.IELM.badger.intel.C60051649f4-F387082946.2010-2011.' -r /lustre/scratch4/turquoise/rutuja/E3SM/scratch -f clm2.h0.2010-12.nc -s 1 -t 2 -g /turquoise/usr/projects/cimmid/users/rutuja/E3SM_cases/proj1/log
+mpiexec -n 2 python /turquoise/usr/projects/higrad/rutuja/DRM/1.FATES-MODEL/src/parallel.run.py -c 'E3SMv2.IELMFATES.badger.intel.C60051649f4-F38708294.2010-2011.' -r /lustre/scratch4/turquoise/rutuja/E3SM/scratch -f clm2.h0.2010-12.nc -s 1 -t 2 -g /turquoise/usr/projects/higrad/rutuja/DRM/1.FATES-MODEL/log
 
 
 #python $PY_SRC_PATH
