cdebfdf37d7ae8200de6c40501bb4ff91db70278
Rutuja Chitra-Tarak <arutuj@gmail.com>
2022-07-14 17:30:58 -0600

    use eglin climate data in prep_elm_parallel.sh

--- 1.FATES-MODEL/src/prep_elm_parallel.sh
+++ 1.FATES-MODEL/src/prep_elm_parallel.sh
@@ -13,7 +13,7 @@ create_variables "$SCRIPT_DIR/../../config.yaml"
 
 # 2.  Unzip sample climate data:
 
-unzip -u data/bci_0.1x0.1_v4.0i/bci_0.1x0.1_met.v4_pio2.zip -d data/bci_0.1x0.1_v4.0i/
+unzip -u data/ConvertMetCSVtoCLM/NCOut/eglin_0.1x0.1_met.v2pio.zip -d data/eglin_0.1x0.1_v4.0i/
 
 # 3. To generate parameter table, if sensitivity analysis:
 
