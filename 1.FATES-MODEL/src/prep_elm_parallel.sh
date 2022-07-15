a0ff7065863f9ba3091a7e19cad906778ce075b6
Rutuja Chitra-Tarak <arutuj@gmail.com>
2022-07-15 08:07:38 -0600

    Only if mutate is true, build/use pram table and generate parameter files in src/prep_elm_parallel.sh

--- 1.FATES-MODEL/src/prep_elm_parallel.sh
+++ 1.FATES-MODEL/src/prep_elm_parallel.sh
@@ -15,21 +15,23 @@ create_variables "$SCRIPT_DIR/../../config.yaml"
 
 unzip -u data/ConvertMetCSVtoCLM/NCOut/eglin_0.1x0.1_met.v2pio.zip -d data/eglin_0.1x0.1_v4.0i/
 
-# 3. To generate parameter table, if sensitivity analysis:
+# 3. If MUTATE is true, then if sensitivity analysis is true, build or use exisitng parameter table:
 
-if [ ${SENSITIVITY} == TRUE ] || [ ${SENSITIVITY} == true ]; then
-        if [ ${BUILD_PARAM_TABLE} == TRUE ] || if [ ${BUILD_PARAM_TABLE} == true ]; then
-                echo "Building Parameter Sensitivity Table"
-		python src/generate.param.table.py
-        else
-                echo "Parameter Sensitivity Table given, Not building"
+if [ ${MUTATE} == TRUE ] || [ ${MUTATE} == true ]; then
+        if [ ${SENSITIVITY} == TRUE ] || [ ${SENSITIVITY} == true ]; then
+                if [ ${BUILD_PARAM_TABLE} == TRUE ] || [ ${BUILD_PARAM_TABLE} == true ]; then
+                        echo "Building Parameter Sensitivity Table"
+                        python src/generate.param.table.py
+                else
+                        echo "Parameter Sensitivity Table given, Not building"
+                fi
         fi
+
+        # 4. To generate multiple parameter files based on paramter ensembles in Param.Table.csv, run:
+
+        python src/generate.inputs.py
 fi
 
-# 4. To generate multiple parameter files based on paramter ensembles in Param.Table.csv, run:
-
-python src/generate.inputs.py
- 
 # 5. To generate a base case of ELM, run:
 
 python src/create.basecase.py
