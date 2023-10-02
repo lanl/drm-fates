8038ad681de129e97234246181539114828e9113
Rutuja Chitra-Tarak <arutuj@gmail.com>
2023-10-02 10:35:29 -0600

    sourcing ~/.tcshrc from run_elm.sh since it's a tcsh file

--- 1.FATES-MODEL/src/run_elm.sh
+++ 1.FATES-MODEL/src/run_elm.sh
@@ -5,7 +5,7 @@
 #SBATCH -N 1 # number of nodes
 #SBATCH -t 00:05:00
 
-. ~/.tcshrc
+source ~/.tcshrc
 
 #conda activate elm_env
 conda activate mpi4pyEnv_GNU
@@ -17,7 +17,7 @@ echo 'Started on '`date`' for '$casedir' starting in '$casedir
 cd $casedir
 
 setenv location `date "+%F-%T"`
-srun -n 4 python /usr/projects/higrad/rutuja/E3SM_cases/proj1/1.FATES-MODEL/src/parallel.run.py -c 'drm.FATES.api24.12.1.1.0.5.IELMFATES.chicoma.gnu.Cc6963c159f-Fbeceb996d.1990-1991.' -r /usr/projects/higrad/rutuja/E3SM.DRM/runs -f elm.h0.1990-12.nc -s 1 -t 4 -g /usr/projects/higrad/rutuja/E3SM_cases/proj1/1.FATES-MODEL/log
+srun -n 4 python /usr/projects/higrad/rutuja/E3SM_cases/proj1/1.FATES-MODEL/src/parallel.run.py -c 'drm.FATES.api24.12.1.1.0.5_3.IELMFATES.chicoma.gnu.Cc6963c159f-Fbeceb996d.1990-1990.' -r /usr/projects/higrad/rutuja/E3SM.DRM/runs -f elm.h0.1990-12.nc -s 1 -t 4 -g /usr/projects/higrad/rutuja/E3SM_cases/proj1/1.FATES-MODEL/log
 
 
 echo 'Finished on '`date`
